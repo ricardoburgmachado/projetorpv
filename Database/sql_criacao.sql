c0135d4b744a2d1412532eecb84634a8ba99bbc9
Rafael Fernando dos Santos <rfs-rafael@hotmail.com>
2013-08-01 19:08:20 -0300

    Correcoes no banco de dados - chaves primarias e estrangeiras

--- Database/sql_criacao.sql
+++ Database/sql_criacao.sql
@@ -1,129 +1,129 @@
 
-CREATE SEQUENCE tipo_projeto_id_tipo_proj_seq_1;
+CREATE SEQUENCE public.tipo_projeto_id_tipo_proj_seq_1;
 
-CREATE TABLE tipo_projeto (
-                id_tipo_proj INTEGER NOT NULL DEFAULT nextval('tipo_projeto_id_tipo_proj_seq_1'),
+CREATE TABLE public.tipo_projeto (
+                id_tipo_proj INTEGER NOT NULL DEFAULT nextval('public.tipo_projeto_id_tipo_proj_seq_1'),
                 descricao VARCHAR(40) NOT NULL,
                 CONSTRAINT id_tipo_proj_pk PRIMARY KEY (id_tipo_proj)
 );
 
 
-ALTER SEQUENCE tipo_projeto_id_tipo_proj_seq_1 OWNED BY tipo_projeto.id_tipo_proj;
+ALTER SEQUENCE public.tipo_projeto_id_tipo_proj_seq_1 OWNED BY public.tipo_projeto.id_tipo_proj;
 
-CREATE SEQUENCE projeto_id_proj_seq;
+CREATE SEQUENCE public.projeto_id_proj_seq;
 
-CREATE TABLE projeto (
-                id_proj INTEGER NOT NULL DEFAULT nextval('projeto_id_proj_seq'),
+CREATE TABLE public.projeto (
+                id_proj INTEGER NOT NULL DEFAULT nextval('public.projeto_id_proj_seq'),
                 id_tipo_proj INTEGER NOT NULL,
                 CONSTRAINT id_pk PRIMARY KEY (id_proj)
 );
 
 
-ALTER SEQUENCE projeto_id_proj_seq OWNED BY projeto.id_proj;
+ALTER SEQUENCE public.projeto_id_proj_seq OWNED BY public.projeto.id_proj;
 
-CREATE SEQUENCE permissao_id_perm_seq;
+CREATE SEQUENCE public.permissao_id_perm_seq;
 
-CREATE TABLE permissao (
-                id_perm INTEGER NOT NULL DEFAULT nextval('permissao_id_perm_seq'),
+CREATE TABLE public.permissao (
+                id_perm INTEGER NOT NULL DEFAULT nextval('public.permissao_id_perm_seq'),
                 descricao VARCHAR(30) NOT NULL,
                 CONSTRAINT id_perm_pk PRIMARY KEY (id_perm)
 );
 
 
-ALTER SEQUENCE permissao_id_perm_seq OWNED BY permissao.id_perm;
+ALTER SEQUENCE public.permissao_id_perm_seq OWNED BY public.permissao.id_perm;
 
-CREATE TABLE usuario (
+CREATE TABLE public.usuario (
                 login VARCHAR(30) NOT NULL,
                 senha VARCHAR(16) NOT NULL,
                 nome VARCHAR(50) NOT NULL,
-                id INTEGER NOT NULL,
+                id_perm INTEGER NOT NULL,
                 CONSTRAINT login_pk PRIMARY KEY (login)
 );
 
 
-CREATE TABLE participante (
-                id INTEGER NOT NULL,
+CREATE TABLE public.participante (
+                id_particip INTEGER NOT NULL,
                 login VARCHAR(30) NOT NULL,
-                CONSTRAINT part_proj_pk PRIMARY KEY (id, login)
+                CONSTRAINT part_proj_pk PRIMARY KEY (id_particip, login)
 );
 
 
-CREATE TABLE coordenador (
+CREATE TABLE public.coordenador (
                 login VARCHAR(30) NOT NULL,
-                CONSTRAINT login_pk PRIMARY KEY (login)
+                CONSTRAINT coordenador_login_ok PRIMARY KEY (login)
 );
 
 
-CREATE TABLE professor (
+CREATE TABLE public.professor (
                 login VARCHAR(30) NOT NULL,
-                CONSTRAINT login_pk PRIMARY KEY (login)
+                CONSTRAINT professor_login_pk PRIMARY KEY (login)
 );
 
 
-CREATE TABLE aluno (
+CREATE TABLE public.aluno (
                 login VARCHAR(30) NOT NULL,
-                CONSTRAINT login_pk PRIMARY KEY (login)
+                CONSTRAINT aluno_login_pk PRIMARY KEY (login)
 );
 
 
-CREATE TABLE pro_reitor (
+CREATE TABLE public.pro_reitor (
                 login VARCHAR(30) NOT NULL,
-                CONSTRAINT login_pk PRIMARY KEY (login)
+                CONSTRAINT pr_reitor_login_pk PRIMARY KEY (login)
 );
 
 
-ALTER TABLE projeto ADD CONSTRAINT tipo_projeto_projeto_fk
+ALTER TABLE public.projeto ADD CONSTRAINT tipo_projeto_projeto_fk
 FOREIGN KEY (id_tipo_proj)
-REFERENCES tipo_projeto (id_tipo_proj)
+REFERENCES public.tipo_projeto (id_tipo_proj)
 ON DELETE RESTRICT
 ON UPDATE RESTRICT
 NOT DEFERRABLE;
 
-ALTER TABLE participante ADD CONSTRAINT projeto_participante_fk
-FOREIGN KEY (id)
-REFERENCES projeto (id_proj)
+ALTER TABLE public.participante ADD CONSTRAINT projeto_participante_fk
+FOREIGN KEY (id_particip)
+REFERENCES public.projeto (id_proj)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE usuario ADD CONSTRAINT tipo_autorizacao_usuario_fk
-FOREIGN KEY (id)
-REFERENCES permissao (id_perm)
+ALTER TABLE public.usuario ADD CONSTRAINT tipo_autorizacao_usuario_fk
+FOREIGN KEY (id_perm)
+REFERENCES public.permissao (id_perm)
 ON DELETE RESTRICT
 ON UPDATE RESTRICT
 NOT DEFERRABLE;
 
-ALTER TABLE pro_reitor ADD CONSTRAINT usuario_pro_reitor_fk
+ALTER TABLE public.pro_reitor ADD CONSTRAINT usuario_pro_reitor_fk
 FOREIGN KEY (login)
-REFERENCES usuario (login)
+REFERENCES public.usuario (login)
 ON DELETE CASCADE
 ON UPDATE CASCADE
 NOT DEFERRABLE;
 
-ALTER TABLE aluno ADD CONSTRAINT usuario_aluno_fk
+ALTER TABLE public.aluno ADD CONSTRAINT usuario_aluno_fk
 FOREIGN KEY (login)
-REFERENCES usuario (login)
+REFERENCES public.usuario (login)
 ON DELETE CASCADE
 ON UPDATE CASCADE
 DEFERRABLE INITIALLY IMMEDIATE;
 
-ALTER TABLE professor ADD CONSTRAINT usuario_professor_fk
+ALTER TABLE public.professor ADD CONSTRAINT usuario_professor_fk
 FOREIGN KEY (login)
-REFERENCES usuario (login)
+REFERENCES public.usuario (login)
 ON DELETE CASCADE
 ON UPDATE CASCADE
 NOT DEFERRABLE;
 
-ALTER TABLE coordenador ADD CONSTRAINT usuario_coordenador_fk
+ALTER TABLE public.coordenador ADD CONSTRAINT usuario_coordenador_fk
 FOREIGN KEY (login)
-REFERENCES usuario (login)
+REFERENCES public.usuario (login)
 ON DELETE CASCADE
 ON UPDATE CASCADE
 NOT DEFERRABLE;
 
-ALTER TABLE participante ADD CONSTRAINT usuario_participante_fk
+ALTER TABLE public.participante ADD CONSTRAINT usuario_participante_fk
 FOREIGN KEY (login)
-REFERENCES usuario (login)
+REFERENCES public.usuario (login)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
