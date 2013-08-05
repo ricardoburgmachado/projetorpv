17d2b6cba4cd3349255282ec838fc41988f602a2
Rafael Fernando dos Santos <rfs-rafael@hotmail.com>
2013-08-05 17:18:29 -0300

    Hibernate Configurado

--- src/java/br/com/dao/UsuarioHibernate.java
+++ src/java/br/com/dao/UsuarioHibernate.java
@@ -6,16 +6,21 @@ package br.com.dao;
 
 import Exceptions.PersistenciaException;
 import br.com.model.Usuario;
+import org.hibernate.Session;
 
 /**
  *
  * @author rafael
  */
-public class UsuarioHibernate implements UsuarioDAO{
+public class UsuarioHibernate implements UsuarioDAO {
+    
+    private Session session;
+
+    public UsuarioHibernate() {
+    }
 
     @Override
     public Usuario autentica(String login, String senha) throws PersistenciaException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
 }
