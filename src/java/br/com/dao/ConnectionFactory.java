b383c411aea719b4f79cd1205700ef55c0fb490b
Rafael Fernando dos Santos <rfs-rafael@hotmail.com>
2013-09-05 21:01:30 -0300

    ConnectionFactory -> fechando conexão

--- src/java/br/com/dao/ConnectionFactory.java
+++ src/java/br/com/dao/ConnectionFactory.java
@@ -1,5 +1,6 @@
 package br.com.dao;
 
+import Exceptions.PersistenciaException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,14 +12,15 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 
     private static ConnectionFactory connectionFactory = new ConnectionFactory();
-    
-    private ConnectionFactory(){}
-    
-    public static ConnectionFactory getInstance(){
-        
+
+    private ConnectionFactory() {
+    }
+
+    public static ConnectionFactory getInstance() {
+
         return connectionFactory;
     }
-    
+
     public Connection createConnectionPostgres() {
 
         Connection con = null;
@@ -29,13 +31,31 @@ public class ConnectionFactory {
             System.out.println("Erro ao obter driver de conexão " + ex);
         }
 
-        try{
+        try {
 
             con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/banco_gipa", "postgres", "postgres");
         } catch (SQLException ex) {
             System.out.println("Erro ao obter conexão " + ex);
         }
-                    
+
         return con;
+    }
+
+    public boolean close(Connection con) throws PersistenciaException {
+
+        if(con == null){
+            
+            return false;
+        }
+        
+        try {
+
+            con.close();
+        } catch (SQLException ex) {
+            
+            throw new PersistenciaException("Falha ao fechar conexão!", ex);
+        }
+        
+        return true;
     }
 }
